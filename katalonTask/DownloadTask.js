c1319b21811566338fe423372629d81e9a7fe57d
Tuan Anh Tran (1765) <tuananhtran@kms.com.vn>
2019-03-11 12:50:50 +0700

    download task

--- katalonTask/DownloadTask.js
+++ katalonTask/DownloadTask.js
@@ -1,64 +1,60 @@
+// @ts-nocheck
 // const constants = require()
 const fs = require('fs');
 const unzip = require('unzip');
 const mkdirp = require('mkdirp');
 const path = require('path');
 const homedir = require('os').homedir();
-const express = require('express');
+// const express = require('express');
 const request = require('request');
-
-const workerDir = path.join(homedir, '.katalon');
 
 const url = "https://github.com/katalon-studio/katalon-studio/releases/download/v5.10.1/Katalon_Studio_Windows_32-5.10.1.zip";
 const name = "Katalon_Studio_Windows_32-5.10.1.zip";
 const folder = "Katalon_Studio_Windows_32-5.10.1";
 const version = "5.10.1";
 
-console.log(workerDir);
+function DownloadAndExtract(version, url, name) {
+    var workerDir = path.join(homedir, '.katalon');
 
-if (!fs.existsSync(workerDir)){
-    // @ts-ignore
-    mkdirp(workerDir);
-}
+    if (!fs.existsSync(workerDir)) {
+        mkdirp(workerDir);
+    }
 
-const destDir = path.join(workerDir, version);
+    var versionDir = path.join(workerDir, version);
+    if (!fs.existsSync(versionDir)) {
+        mkdirp(versionDir);
+    }
 
-const dirFileZip = path.join(destDir, name);
+    var fileZipDir = path.join(versionDir, name);
+    var katalonFolder = path.join(versionDir, getName(name));
+    console.log(versionDir);
 
-if (!fs.existsSync(destDir)){
-    // @ts-ignore
-    mkdirp(destDir);
-}
-console.log("Get forder to save katalon done");
+    // request(url).pipe(fs.createWriteStream(fileZipDir));
 
-download = function(callback){
-    request(url).pipe(fs.createWriteStream(dirFileZip));
-    return callback = true;
-};
+    request(url).pipe(fs.createWriteStream(fileZipDir, (err) => {
+        if (err) throw err;
 
-extract = function(callback){
-    fs.createReadStream(dirFileZip).pipe(unzip.Extract({ path: destDir }));
-    return callback = true;
-}
+        console.log("Download zip done");
 
-var toDownload = download(function(callback){
-    console.log("Extractingg");
-    extract(function(callback){
-        fs.unlink(toSave, (err) => {
+        fs.createReadStream(versionDir).pipe(unzip.Extract({path: destDir}, (err) => {
             if (err) throw err;
-            console.log('Deleted done');
-          });
-    });
-    console.log("Download done!");
-});
 
-const folderDirKatalon = path.join(destDir, folder);
+            console.log("Extract done");
+            fs.unlink(fileZipDir, (err) => {
+                if (err) throw err;
+                console.log("Deleted zip done");
+            })
+            fs.writeFile(path.join(katalonFolder, ".done"), "Download done at " + new Date(), (err) => {
+                if (err) throw err;
+                console.log("Done");
+            });
+        }));
+    }));
+}
 
-// Notification to download and extract done
-// var done = fs.writeFile(path.join(folderDirKatalon, ".done"), "Downloaded at " + new Date(), function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//     console.log("The file was saved!");
-// }); 
+function getName(name) {
+    var indexOf = name.lastIndexOf(".");
+    return name.substring(0, indexOf);
+}
 
+DownloadAndExtract(version, url, name);
